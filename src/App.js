8ef9490618b4989f984e6d57f908a21281201d98
Logan3520 <166590086+Logan3520@users.noreply.github.com>
2024-09-17 23:29:24 +0530

    table creation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
+import React from 'react'
 
 
+function App() {
 
-import { Button } from "react-bootstrap";
-
-function App(){
-  return(
-    <>
-    <div className="App">
-      <h1>Install Booostrap</h1>
-      <Button>Click me</Button>
-      <Button>Manish view</Button>
+  const Student =[
+    {name:"manish", email:"manishsonawane18s@gmail.com", contact:888},
+    {name:"Nikita", email:"nikitasonawane13ns@gmail.com", contact:111},
+    {name:"manisha", email:"manishasonawane28s@gmail.com", contact:100},
+  ]
+  return (
+    <div>
+      <h1>Hnadeling list of members</h1>
+      <table>
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Contact</td>
+          </tr>
+         { Student.map((element)=>
+          <tr>
+            <td>{element.name}</td>
+            <td>{element.email}</td>
+            <td>{element.contact}</td>
+          </tr>
+)}
+      </table>
     </div>
-    </>
   )
 }
 
 export default App
-
-rfce
